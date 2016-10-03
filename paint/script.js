ac642a69181b62c2d8f3bed51bcc8e7a55fc3f5c
Ryan Impey <ryanimpey@live.co.uk>
2016-10-03 17:23:21 +0100

    HTML Update #1
    Created responsive dark nav, included navbar-brand image and created a
    colour picker modal for the start of the paintbrush

--- paint/script.js
+++ paint/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-	$('h1').hover(function(){
-		//alert('hello');		
-	})
+	/*$('h1').hover(function(){
+		alert('hello');		
+	})*/
+
+	// Initialize tooltip component
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// Initialize popover component
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
 });
