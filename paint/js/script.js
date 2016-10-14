a38b1089f3a5637404997872dd6e783a8d9206e3
Ryan Impey <ryanimpey@live.co.uk>
2016-10-14 11:36:50 +0100

    commit

--- paint/js/script.js
+++ paint/js/script.js
@@ -1,12 +1,24 @@
-var valueofNumber = 0;
 $(document).ready(function(){
 
 	$('.btn-primary-submit-canvas').click(function(){
-		//$('#paintboard').width()
+
 		var canvasWidthVar = document.getElementById("canvasWidth").value;
 		var canvasHeightVar = document.getElementById("canvasHeight").value;
-		
-		console.log(canvasWidthVar);
-		console.log(canvasHeightVar);
+
+		console.log("width:" + canvasWidthVar + " height:" + canvasHeightVar);
+
+		//$('#paintboard').width()
+		if (canvasWidthVar > 4999 || canvasHeightVar > 4999) {
+			if (confirm("You are creating a large canvas, are you sure you want to do this?") == true) {
+				changeCanvas(canvasWidthVar, canvasHeightVar);
+			}
+		} else {
+			changeCanvas(canvasWidthVar, canvasHeightVar);
+		}
+		$('#canvasCreatedModal').modal('hide')
 	});
+
+	function changeCanvas(canvasWidthVar, canvasHeightVar) {
+		$('#paint-board').width(canvasWidthVar).height(canvasHeightVar);
+	}
 });
