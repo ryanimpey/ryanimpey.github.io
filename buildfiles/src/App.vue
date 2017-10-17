a964e23550952b6a569a3eb95c4e4ef9b3024c5d
Ryan Impey <ryanimpey@live.co.uk>
2017-10-17 21:15:05 +0100

    Added vue-cli stuff and removed Octogoin holder
    Major overhaul for an actual cv site and replaced the octogoin stuff

---
+++ buildfiles/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Welcome />
+  </div>
+</template>
+
+<script>
+import Welcome from './components/Welcome'
+
+export default {
+  name: 'app',
+  components: {
+    Welcome
+  }
+}
+</script>
+
+<style>
+</style>
