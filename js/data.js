9f41c6b2754fe9468f765e91788ae25635e4e7f2
Ryan <ryanimpey@live.co.uk>
2017-11-10 15:50:55 +0000

    minor portfolio changes
    Layout changed to allow text area

---
+++ js/data.js
@@ -0,0 +1,28 @@
+const portfolio = [
+  {
+    "Title": "Univeristy of Lincoln",
+    "Description": [
+      "Part of the student developer team building web applications to aid students in daily activities, using technologies such as HTML, CSS, Javascript and related libraries",
+      "Learning new frameworks and libraries whilst working to provide a better service for the finished application",
+      "Aided and taught other members of the team how to use libraries and programs by providing documentation and support to help increase productivity"
+    ],
+    "Tech": ["HTML", "CSS", "JS", "C#", "JSON", "Git", "LeafletJS"],
+  },
+  {
+    "Title": "Vue CS:GO Stats",
+    "Description": "aaa",
+    "Tech": [],
+  },
+  {
+    "Title": "LincHack Cat Facts",
+    "Description": "aaa",
+    "Tech": [],
+  },
+  {
+    "Title": "Octgoin",
+    "Description": "aaa",
+    "Tech": [],
+  },
+];
+
+export {portfolio};
